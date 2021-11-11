515810be509e9cefa4714c027b8e727f7bbf1e30
Edmund Krain <75438446+minerop5000@users.noreply.github.com>
2021-11-11 19:48:28 +0100

    fix: backend-unique username

--- basic-backend/models/db.ts
+++ basic-backend/models/db.ts
@@ -1,3 +1,6 @@
 import Datastore from 'nedb';
 export const echo = new Datastore({ filename: './databases/echo.db', autoload: true });
 export const user = new Datastore({ filename: './databases/user.db', autoload: true });
+// user.ensureIndex({fieldName: "a", a: 1}, {unique: true})
+user.ensureIndex({unique: true, fieldName: "username"})
+
