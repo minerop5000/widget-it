579618782a89739826f0ac28c9c2cf4e82b0072f
Edmund <75438446+minerop5000@users.noreply.github.com>
2021-12-09 23:54:38 +0100

    module basic design

--- basic-frontend/src/app/pages/welcome/welcome.module.ts
+++ basic-frontend/src/app/pages/welcome/welcome.module.ts
@@ -6,6 +6,12 @@ import {SharedModule} from "../../shared/shared.module";
 import { ModulComponent } from './modul/modul.component';
 import { CreatorComponent } from './creator/creator.component';
 import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzSpaceModule} from "ng-zorro-antd/space";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzMenuModule} from "ng-zorro-antd/menu";
+import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 @NgModule({
@@ -17,7 +23,13 @@ import {NzIconModule} from "ng-zorro-antd/icon";
   imports: [
     SharedModule,
     WelcomeRoutingModule,
-    NzIconModule
+    NzIconModule,
+    NzSpaceModule,
+    NzCardModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzButtonModule
   ]
 })
 export class WelcomeModule {
